23e75b31f3cb61561f4d3c9416276dc2d6183c1a
Danibs2205 <danibs2109@gmail.com>
2026-03-07 17:04:18 -0300

    fix(footer): change contact link to biolink

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -43,7 +43,7 @@ export const Footer: React.FC = () => {
               <li><a href="/#metodo" className="hover:text-sky-600 transition-colors">Como funciona</a></li>
               <li><a href="/#beneficios" className="hover:text-sky-600 transition-colors">Benefícios</a></li>
               <li><Link to="/materiais" className="hover:text-sky-600 transition-colors">Materiais</Link></li>
-              <li><a href="https://wa.me/5575983507283" className="hover:text-sky-600 transition-colors">Contato</a></li>
+              <li><a href="https://biolink.website/leianeves_psicopedagoga" target="_blank" rel="noopener noreferrer" className="hover:text-sky-600 transition-colors">Contato</a></li>
             </ul>
           </div>
           <div>
